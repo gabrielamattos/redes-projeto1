fd59a530c8a0e0148cbc8d544b2d598db1d8209b
BrenoSS <BrenoSS@users.noreply.github.com>
2015-10-03 16:00:48 -0300

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -6,21 +6,23 @@ serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
-print "The server is ready to receive"
-x = "|"
-y = ";"
-z = ">"
+a = "|"
+b = ";"
+c = ">"
+d = "<"
 while 1:
 	connectionSocket, addr = serverSocket.accept()
      
 	sentence = connectionSocket.recv(1024)
 	verificacao = sentence.startswith('REQUEST')
-	if x in sentence
-		sentence.replace(x,"")
-	if y in sentence
-		sentence.replace(y,"")
-	if z in sentence
-		sentence.replace(z,"")
+	if a in sentence
+		sentence.replace(a,"")
+	if b in sentence
+		sentence.replace(b,"")
+	if c in sentence
+		sentence.replace(c,"")
+	if d in sentence
+		sentence.replace(d,"")
 
 	comando = subprocess.Popen(sentence, stdout=subprocess.PIPE, shell=True)
 	(resposta, err) = comando.communicate()
