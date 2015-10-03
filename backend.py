ca55c14ef1579c74732390aa83753762c64d8440
rophos-rb <ro_barcelar@yahoo.com.br>
2015-10-03 16:51:03 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,1 +1,32 @@
+#funcionará como um cliente, ou seja, pega o valor passado pelo WebServer e envia para os computadores os executarem
+import socket
 
+#definição do comando a ser chamado pelo WebServer
+def sendMsg(comando):
+  serverName = '192.168.0.1'
+  serverPort = 9003 #aqui, temos 9000 + X, sendo X o número do grupo. Mas não sei que número é nosso grupo
+  clientSocket = socket(AF_INET,SOCK_STREAM)
+  try:
+    clientSocket.connect ((serverName, serverPort))
+    if comando:
+      #envio do comando recebido para o servidor
+      clientSocket.send(comando)
+      #devo limpar o comando
+      comando = ""
+      dados = clientSocket.rcv(1024)
+      dados = validacaoDosDados(dados)
+    else:
+      dados = "Impossível gerar comando!"
+    clientSocket.close()
+  except Excecao:
+    dados = "Socket sem conexão!!"
+    
+    return dados
+    
+  def validacaoDosDados(msgRecebida):
+    lista = msgRecebida.split() #comando split() separa cada palavra contida na mensagem
+    if lista[0] != "RESPONSE":
+        return "Resposta enviada é incoerente!"
+    lista = msgRecebida.split("RESPONSE") #pega tudo que tem na mensagem recebida, menos 'RESPONSE'   
+    return  lista[1]
+    
