124581813897f1015005fe38bd33631bbe5db2f1
BrenoSS <BrenoSS@users.noreply.github.com>
2015-10-03 16:41:28 -0300

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -10,11 +10,16 @@ a = "|"
 b = ";"
 c = ">"
 d = "<"
+e = "1 "
+f = "2 "
+g = "3 "
+h = "4 "
 while 1:
 	connectionSocket, addr = serverSocket.accept()
      
 	sentence = connectionSocket.recv(1024)
-	verificacao = sentence.startswith('REQUEST')
+	verificacao = sentence.startswith("REQUEST")
+	sentence.replace("REQUEST ","")
 	if a in sentence
 		sentence.replace(a,"")
 	if b in sentence
@@ -23,9 +28,18 @@ while 1:
 		sentence.replace(c,"")
 	if d in sentence
 		sentence.replace(d,"")
+	if e in sentence
+		sentence.replace(e,"ps -")
+	if f in sentence
+		sentence.replace(f,"ds -")
+	if g in sentence
+		sentence.replace(g,"finger -")
+	if h in sentence
+		sentence.replace(h,"uptime -")
+	
 
 	comando = subprocess.Popen(sentence, stdout=subprocess.PIPE, shell=True)
 	(resposta, err) = comando.communicate()
-	resposta = "RESPONSE" + resposta
+	resposta = "RESPONSE " + resposta
 	connectionSocket.send(resposta)
 	connectionSocket.close()
