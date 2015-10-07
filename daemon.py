736f00f7cb1143377c1405d319d32dc7b4d37d1c
BrenoSS <BrenoSS@users.noreply.github.com>
2015-10-06 22:11:57 -0300

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -13,26 +13,32 @@
 from socket import *
 import subprocess
 import string
-import thread
 
-def conexao(con, cliente):
-	#estabelecendo os padroes a serem procurados na string, os 4 primeiros devem ser ignorados, os 4 ultimos substituidos
-	#pelo comando equivalente
-	a = "|"
-	b = ";"
-	c = ">"
-	d = "<"
-	e = "1 "
-	f = "2 "
-	g = "3 "
-	h = "4 "
-	#Recebe a sentenca do cliente
-	sentence = con.recv(1024)
+#Estabelecendo a porta
+serverPort = 9003
+#Criando socket TCP
+serverSocket = socket(AF_INET,SOCK_STREAM)
+#Associando a porta 9003 com o socket do servidor
+serverSocket.bind(("",serverPort))
+#Espera pelos pacotes do cliente
+serverSocket.listen(1)
+a = "|"
+b = ";"
+c = ">"
+d = "<"
+e = "1 "
+f = "2 "
+g = "3 "
+h = "4 "
+
+
+while 1:
+	connectionSocket, addr = serverSocket.accept()
+	sentence = connectionSocket.recv(1024)
 	if len(sentence) > 10:
 		menos = "-"
 	else:
 		menos = ""
-	#fazendo as substituicoes necessarias nos padroes
 	sentence = sentence.replace("REQUEST ","")
 	if a in sentence:
 		sentence = sentence.replace(a,"")
@@ -45,43 +51,18 @@ def conexao(con, cliente):
 	if e in sentence:
 		sentence = sentence.replace(e,"ps "+menos)
 		numero = e
-	elif f in sentence:
+	if f in sentence:
 		sentence = sentence.replace(f,"ds "+menos)
 		numero = f
-	elif g in sentence:
+	if g in sentence:
 		sentence = sentence.replace(g,"finger "+menos)
 		numero = g
-	elif h in sentence:
+	if h in sentence:
 		sentence = sentence.replace(h,"uptime "+menos)
 		numero = h
-	#executando um processo filho com Ponpen, passando a sentenca tratada como parametro
 	comando = subprocess.Popen(sentence, stdout=subprocess.PIPE, shell=True)
-	#associando a saida com uma variavel de saida
 	(resposta, err) = comando.communicate()
-	#formulando o cabecalo de reposta de acordo com o padrao
 	resposta = "RESPONSE " + numero + resposta
-	#enviando a resposta
-	con.send(resposta)
-	#fechando a conexao
-	con.close()	
-	thread.exit()
-
-
-#Estabelecendo a porta
-serverPort = 9003
-#Criando socket TCP
-serverSocket = socket(AF_INET,SOCK_STREAM)
-#Associando a porta 9003 com o socket do servidor
-serverSocket.bind(("",serverPort))
-#Espera pelos pacotes do cliente
-serverSocket.listen(1)
-
-
-while 1:
-	#quando o cliente bate a porta, o serverSocket chama o metodo accept
-	#e cria um novo socket no servidor chamado connectionSocket que e dedicado a esse cliente
-	connectionSocket, addr = serverSocket.accept()
-	#inicia uma nova thread para tratar requisicao do cliente
-	thread.start_new_thread(conexao, tuple([connectionSocket, addr]))
-
+	connectionSocket.send(resposta)
+	connectionSocket.close()
 
