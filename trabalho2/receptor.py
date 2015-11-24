0c9893d76f7adb3293d3a4a38d63292ab1e990ae
gabrielamattos <gaby.vanessa@gmail.com>
2015-11-24 09:44:23 -0200

    Correção de alguns erros

--- trabalho2/receptor.py
+++ trabalho2/receptor.py
@@ -8,19 +8,25 @@ import sys
 def main():
 #recuperando agumentos da linha de comando
 #>receptor <hostname do rementente> <numero de porta do rementente> <nome do arquivo>
-	hostname = sys.argv[1]
-	numPortRem = sys.argv[2]
-	nomeArq = sys.argv[3]
 	
-	recebendo = 1
-	while recebendo:
+	if (len(sys.argv) > 3):
+		hostname = sys.argv[1]
+		numPortRem = int(sys.argv[2])
+		nomeArq = sys.argv[3]
 	
-	receptorSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
-	clientSocket.sendto(nomeArq,(hostname, numPortRem))
-	respostas = clientSocket.recvfrom(2048)
+		recebendo = 1
+		while recebendo:
+	
+			receptorSocket = socket(AF_INET, SOCK_DGRAM)
+			receptorSocket.sendto(nomeArq,(hostname, numPortRem))
+			respostas = receptorSocket.recvfrom(2048)
 
-	#if resposta fim da mensagem recebendo = 0
+			#if resposta fim da mensagem recebendo = 0
 
-clientSocket.close()
+		receptorSocket.close()
+	else:
+		print "Espera-se os argumentos: hostname do rementente, numero de porta do rementente e nome do arquivo."
+
+
 
 main()
