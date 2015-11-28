4f214cb164411f85939673f23175cfdbb907047d
BrenoSS <BrenoSS@users.noreply.github.com>
2015-11-27 18:47:46 -0800

    Update topologia.py

--- trabalho2/topologia.py
+++ trabalho2/topologia.py
@@ -1,15 +1,31 @@
-from mininet import *
-net = Mininet() # net is a Mininet() object
-h1 = net.addHost( 'h1' ) # h1 is a Host() object
-h2 = net.addHost( 'h2' ) # h2 is a Host()
-s1 = net.addSwitch( 's1' ) # s1 is a Switch() object
-c0 = net.addController( 'c0' ) # c0 is a Controller()
-net.addLink( h1, s1 ) # creates a Link() object
-net.addLink( h2, s1 )
-net.start()
-h2.cmd( 'python -m SimpleHTTPServer 80 &' )
-sleep( 2 )
-h1.cmd( 'curl', h2.IP() )
-CLI( net )
-h2.cmd('kill %python')
-net.stop() 
+#!/usr/bin/python
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import irange,dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.cli import CLI
+
+
+
+def simpleTest():
+	"Create and test a simple network"
+	
+	net = Mininet(host=CPULimitedHost, link=TCLink)
+	h1 = net.addHost( 'h1' ) # h1 is a Host() object
+	h2 = net.addHost( 'h2' ) # h2 is a Host()
+	#s1 = net.addSwitch( 's1' ) # s1 is a Switch() object
+	c0 = net.addController( 'c0' ) # c0 is a Controller()
+	net.addLink( h1, h2, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True ) # creates a Link() object
+	#net.addLink( h2, s1, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True )
+	net.start()
+	#h2.cmd( 'python receptor.py &' )
+	#h1.cmd( 'python emissor.py &'  )
+	CLI( net )
+	net.stop()
+	
+	
+
+
+simpleTest()
