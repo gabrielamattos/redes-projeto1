2d92c829ee1a37f2ddbfd4408e2be13bc423fb00
BrenoSS <BrenoSS@users.noreply.github.com>
2015-11-28 17:27:56 -0800

    Update topologia.py

--- trabalho2/topologia.py
+++ trabalho2/topologia.py
@@ -13,15 +13,15 @@ def simpleTest():
 	"Create and test a simple network"
 	
 	net = Mininet(host=CPULimitedHost, link=TCLink)
-	h1 = net.addHost( 'h1' ) # h1 is a Host() object
-	h2 = net.addHost( 'h2' ) # h2 is a Host()
+	h1 = net.addHost( 'h1', ip='10.0.0.1' ) # h1 is a Host() object
+	h2 = net.addHost( 'h2', ip='10.0.0.2' ) # h2 is a Host()
 	#s1 = net.addSwitch( 's1' ) # s1 is a Switch() object
 	c0 = net.addController( 'c0' ) # c0 is a Controller()
 	net.addLink( h1, h2, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True ) # creates a Link() object
 	#net.addLink( h2, s1, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True )
 	net.start()
-	#h2.cmd( 'python receptor.py &' )
-	#h1.cmd( 'python emissor.py &'  )
+	#h2.cmd( 'nohup python2.7 ./receptor.py & ' )
+	#h1.cmd( 'nohup python2.7 ./emissor.py & '  )
 	CLI( net )
 	net.stop()
 	
