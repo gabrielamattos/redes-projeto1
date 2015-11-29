4910950cce1c591a19879378e5b4efb627f445d2
BrenoSS <BrenoSS@users.noreply.github.com>
2015-11-28 17:27:21 -0800

    Update receptor.py

--- trabalho2/receptor.py
+++ trabalho2/receptor.py
@@ -14,53 +14,40 @@ def checksum(msg,flag):
 	for i in range(0, len(msg),2):
 		if flag == 0:
 			val2 = ord(msg[i]) + (ord(msg[i+1])<<8)
-		elif flag ==1
+		elif flag ==1:
 			val2 = ord('0') + (ord(msg[i+1])<<8)
 		val1 = verCarry(val1,val2)
 	ret = ~val1 & 0xffff
 	return ret
 
-#funcao checksum do livro  capitulo 3 para 16 bits usado no ud para saber se foi alterado indevidademente
-#https://pt.wikiversity.org/wiki/Introdu%C3%A7%C3%A3o_%C3%A0s_Redes_de_Computadores/Protocolo_UDP
-#
+#!/Python27/python
+# -*- coding: utf-8 -*-
+from socket import *
+import sys
+
+#http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
 
 
 
 def main():
-#recuperando agumentos da linha de comando
-#>receptor <hostname do rementente> <numero de porta do rementente> <nome do arquivo>
 	
 	if (len(sys.argv) > 3):
 		nomeHost = sys.argv[1]
 		numPort = int(sys.argv[2])
 		nomeArq = sys.argv[3]
-		#WindowSize = int(sys.argv[4])
-		#Maxdata =  int(sys.argv[5])
-		#tamanho da nossa janela 
-		#tamanho maximo do arquivo
 		print nomeHost
 		print numPort
 		print nomeArq
-		recebendo = 1
-		#Esse while deverá ser usado para receber os pacotes do servidor, logo deve pensar um pouco mais em como ele deve ser usado para receber os pacotes aos poucos, acredito que a solicitação do arquivo (sendto) só pode acontecer uma vez
-		#while recebendo: 
-	
-		receptorSocket = socket(AF_INET, SOCK_DGRAM)
-		receptorSocket.sendto(nomeArq,(nomeHost, numPort))
-		respostas, enderecoServidor = receptorSocket.recvfrom(2048)
-		print respostas
-			#if resposta fim da mensagem recebendo = 0
-			
-		packList = [] 
-		#pacotes a receber
-		npackets = 0
-		#nro pacotes
-		
-		
+		clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-		receptorSocket.close()
+		clientSocket.sendto(nomeArq, (nomeHost, numPort))
+
+		resMessage = clientSocket.recvfrom(8192)[0]
+		clientSocket.close()
+		print resMessage
 	else:
 		print "Espera-se os argumentos: hostname do rementente, numero de porta do rementente e nome do arquivo."
+
 
 
 
