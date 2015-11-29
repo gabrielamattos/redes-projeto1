4700f49c458d313c2d020e40954385663fb6ec24
gabrielamattos <gaby.vanessa@gmail.com>
2015-11-29 11:56:07 -0200

    corrigindo delete errado

--- trabalho1/cgi-bin/backend.py
+++ trabalho1/cgi-bin/backend.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*- 
 
-###############################################################################################################
-	Nome: backend.py											#
-	Autor: Rodolfo Barcelar											#
-      Contribuição: Gabriela Mattos										#
-	Objetivo: funciona como um cliente, ou seja, pega o valor passado pelo WebServer e envia para os 	#
-computadores os executarem.											#
-	Referências:												#
-- https://docs.python.org/2/howto/sockets.html 								#
-- http://wiki.python.org.br/SocketBasico 									#
+#################################################################################################################
+# 	Nome: backend.py											#
+# 	Autor: Rodolfo Barcelar											#
+#	Contribuição: Gabriela Mattos										#
+# 	Objetivo: funciona como um cliente, ou seja, pega o valor passado pelo WebServer e envia para os 	#
+# computadores os executarem.											#
+# 	Referências:												#
+# - https://docs.python.org/2/howto/sockets.html 								#
+# - http://wiki.python.org.br/SocketBasico 									#
 #################################################################################################################
 
 # Importando biblioteca do Socket
@@ -25,35 +25,31 @@ def sendMsg(comando):
 	clientSocket = socket(AF_INET,SOCK_STREAM)
 	
 	# Tentando conectar com o servidor
-	try: 
-    		clientSocket.connect(serverName, serverPort)
-    		if comando:
-      			# Enviando o comando recebido para o servidor... comando TRY..EXCEPT
-      			clientSocket.send(comando)
-      			# Limpando o comando após o envio
-			comando = ""
-			# Recebendo o comando do servidor
-			dados = clientSocket.rcv(1024) 
-			# Validando se a resposta é coerente
-			dados = validacaoDosDados(dados) 
-		else:
-			dados = "Impossivel gerar comando!"
+	clientSocket.connect(serverName, serverPort)
+    	if comando:
+      		# Enviando o comando recebido para o servidor... comando TRY..EXCEPT
+      		clientSocket.send(comando)
+      		# Limpando o comando após o envio
+		comando = ""
+		# Recebendo o comando do servidor
+		dados = clientSocket.rcv(1024) 
+		# Validando se a resposta é coerente
+		dados = validacaoDosDados(dados) 
+	else:
+		dados = "Impossivel gerar comando!"
 
-		clientSocket.close()
+	clientSocket.close()
 
-	except Excecao: #caso a conexao com socket tiver fechado ou nao tiver sido feita
-		dados = "Socket sem conexao!"
- 
 	return dados
 
 def validacaoDosDados(msgRecebida):
-	lista = msgRecebida.split() #comando split() separa cada palavra contida na mensagem
+	
+	verificacao = msgRecebida.find('RESPONSE')
+	tam = len(msgRecebida)
 
-	if lista[0] != "RESPONSE":
-        	return "Resposta enviada eh incoerente!"
+	if verificacao != -1:
+        	return "Resposta enviada eh incoerente!"	
 
-	lista = msgRecebida.split("RESPONSE") #pega tudo que tem na mensagem recebida, menos 'RESPONSE'   
+	resposta = msgRecebida[11:tam]
 
-	return  lista[1]
-    
-
+	return  resposta
