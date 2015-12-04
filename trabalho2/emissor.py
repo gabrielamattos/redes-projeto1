c8bd0c78ff98034b537ade7695515e7a3c4b4080
Gabriela Mattos <gaby.vanessa@gmail.com>
2015-12-04 16:15:35 -0200

    Update emissor.py

--- trabalho2/emissor.py
+++ trabalho2/emissor.py
@@ -5,6 +5,19 @@ from socket import *
 import sys
 import math
 import numpy
+import threading
+import signal
+
+#variaveis globais que serão usadas para enviar os dados e receber o ack
+tamanhoJanela = 2
+numSeqMax = tamanhoJanela - 1
+tamanhoPacote = 5
+ack = 0
+numSeq = 0
+numSeqBase = 0
+
+timeout = 2
+
 
 
 # A mensagem será dividida no tamanho da janela e salva em um vetor de string, o indice do vetor vai representar
@@ -21,11 +34,22 @@ def dividirMensagem(tamanhoPacote, mensagem):
 
 	return pacotes
 
+def receberAck():
+
+	mensagem = servidorSocket.recvfrom(2048)[0]
+
+	parts  = mensagem.split(";")
+	ack = int(parts[0])
+	print ack
+	print numSeq
+	
+	# verificar se não é necessário fazer uso do >=
+	if (ack > numSeqBase):
+		numSeqMax = numSeqMax + (ack+1 - numSeqBase)
+		numSeqBase = ack
+
+
 def main():
-	tamanhoJanela = 2
-	numSeqMax = tamanhoJanela - 1
-	tamanhoPacote = 5
-	ack = 0
 
 	if(len(sys.argv)>1):
 		numPort = int(sys.argv[1])
@@ -38,46 +62,37 @@ def main():
 
 	
 			mensagem, enderecoReceptor = servidorSocket.recvfrom(2048)
-		
-			if (len(mensagem) != 0):
-				
-				numSeqBase = 0
-				numSeq = 0
-				ultimoAck = 0
-				pacotes = dividirMensagem(tamanhoPacote, mensagem)
-				numSeqMax = tamanhoJanela - 1
 
-				#pacote é transmitido em ordem
-				for i in range (0, len(pacotes)):
-		
-					if (numSeq <= numSeqMax):
-					
-						res = str(numSeq) + ";" + pacotes[i] + ";"
-						# [melhorar depois] mensagem avisando que foi enviado o pacote
-						print "Enviando pacote de dados com cabecalho: " + res + "/" + str(len(pacotes))				    
-				        	servidorSocket.sendto(res, enderecoReceptor)
+			numSeqBase = 0
+			numSeq = 0
+			numSeqMax = tamanhoJanela - 1
 
-						numSeq += 1
-					#quando atinge o tamanho da janela ele deve reenviar os pacotes
-					else:
-						print "Tamanho da janela atingido " + str(i)
-						i = ack + 1
-						numSeq = i
-	
-					mensagem = servidorSocket.recvfrom(2048)[0]
+			pacotes = dividirMensagem(tamanhoPacote, mensagem)
 
-					if (len(mensagem) != 0):
-						parts  = mensagem.split(";")
-						ack = int(parts[0])
-						print ack
-						print numSeq
-						#tratar o ack aqui
-						if (ack > numSeqBase):
-							numSeqMax = numSeqMax + (ack+1 - numSeqBase)
-							numSeqBase = ack
-							ultimoAck = ack
-						#duvida: aqui é um tempo ou posso verificar o ultimo?
-						#elif (ack == ultimoAck): #é nesse momento que vamos saber se houve perca, pq o mesmo ack tá sendo enviado mais de uma vez
+			#é criado uma thread para receber os acks do receptor
+			t_receptor = threading.Thread(receberAck)
+			t_receptor.daemon = True
+			t_receptor.start()
+
+			#pacote é transmitido em ordem
+			for i in range (0, len(pacotes)):
+
+				if (numSeq <= numSeqMax):
+			
+					res = str(numSeq) + ";" + pacotes[i] + ";"
+					# [melhorar depois] mensagem avisando que foi enviado o pacote
+					print "Enviando pacote de dados com cabecalho: " + res + "/" + str(len(pacotes))				    
+					servidorSocket.sendto(res, enderecoReceptor)
+
+					numSeq += 1
+				#quando atinge o tamanho da janela ele deve reenviar os pacotes
+				else:
+					#time
+					print "Tamanho da janela atingido " + str(i)
+					i = ack + 1
+					numSeq = i
+
+			
 						
 		else:
 			print "Espera-se o seguinte parametro: numero de porta do serviço"
