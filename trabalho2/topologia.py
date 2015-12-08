1b0ffce70cdb208eecb8e550f6d172fa4dca16ea
BrenoSS <BrenoSS@users.noreply.github.com>
2015-12-08 21:00:25 -0200

    Atualizando topologia

--- trabalho2/topologia.py
+++ trabalho2/topologia.py
@@ -8,6 +8,7 @@ from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 from mininet.cli import CLI
 
+# A topologia foi criada com base no material passado em aula no laboratório de mininet
 # Modo de Usar:
 # Abrir um terminal, mudar o diretório de trabalho para a pasta que contém todos os arquivos
 # No terminal, digitar: sudo python topologia.py
@@ -19,23 +20,17 @@ from mininet.cli import CLI
 # passar como nome do host emissor o nome definido na topologia.
 
 
-def simpleTest():
-
-	
+def topologia():
 	net = Mininet(host=CPULimitedHost, link=TCLink)
 	h1 = net.addHost( 'h1', ip='10.0.0.1' ) # h1 is a Host() object
 	h2 = net.addHost( 'h2', ip='10.0.0.2' ) # h2 is a Host()
-	#s1 = net.addSwitch( 's1' ) # s1 is a Switch() object
 	c0 = net.addController( 'c0' ) # c0 is a Controller()
 	net.addLink( h1, h2, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True ) # creates a Link() object
-	#net.addLink( h2, s1, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True )
 	net.start()
-	#h2.cmd( 'nohup python2.7 ./receptor.py & ' )
-	#h1.cmd( 'nohup python2.7 ./emissor.py & '  )
 	CLI( net )
 	net.stop()
 	
 	
 
 
-simpleTest()
+topologia()
