ee03d75e7e4ba3ca709b10a68d53cabcd09b2be3
Gabriela Mattos <gaby.vanessa@gmail.com>
2015-12-08 04:33:03 -0800

    Emissor funcionando

--- trabalho2/emissor.py
+++ trabalho2/emissor.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
-# Referencias: http://www.cs.sfu.ca/CourseCentral/371/oba2/Sep19.pdf
-#http://www.binarytides.com/programming-udp-sockets-in-python/
+
+#################################################################################################################
+# 	Nome: emissor.py											#
+# 	Objetivo: atua como servidor UDP com adicao dos servicos de checksum e uso do protocolo Go-Back-N	#
+# 	Referencias:												#
+# - http://www.cs.sfu.ca/CourseCentral/371/oba2/Sep19.pdf							#
+# - http://www.binarytides.com/programming-udp-sockets-in-python/						#
+#################################################################################################################
+
+
 from socket import *
 import sys
 import math
@@ -24,44 +32,55 @@ def checksum(msg, opcao):
    		return ~s & 0xffff
 	else:
 		return s & 0xffff
-   
-
-
+  
 
 #variaveis globais que serao usadas para enviar os dados e receber o ack
+pacotes = []
 tamanhoJanela = 2
 numSeqMax = tamanhoJanela - 1
-tamanhoPacote = 5
-ack = -1
 numSeq = 0
 numSeqBase = 0
-i = 0
-checkSum = 0
-look = threading.Lock()
-pacotes = []
+end = 0
+timeout = 2
+ack = -1
 
-timeout = 5
+lock = threading.Lock()
+servidorSocket = socket(AF_INET, SOCK_DGRAM)
 
+
+# Nome da funcao: gerarMensagem()
+# Objetivo: com os valores de número de sequencia e o pacote enviados por parametro, essa funcao vai gerar a mensagem final com checksum
+def gerarMensagem(numS, pacote):
+	pacoteSemCheckSum = str(numS) + ";" + pacote + ";"
+	valorCheckSum = checksum(pacoteSemCheckSum, 0)
+	res = str(valorCheckSum) + ";" + pacoteSemCheckSum
+
+	return res
+
+# Nome da funcao: definirReenvio()
+# Objetivo: quando atinge o timeout, essa funcao trata de reenviar os pacotes da janela		
 def definirReenvio(signum, stack):
 
-	look.acquire()
-	global timeout
-	print 'Houve timeout'
-	global i
-	global numSeq
-	i = ack + 1
-	numSeq  = i
-	print i		
-	print numSeq
 	signal.alarm(timeout)
+	#ack eh a variavel que representa o ultimo ack confirmado
+	for x in range(numSeqBase, numSeqMax+1):
+		res = gerarMensagem(x, pacotes[x])
+		print "Reenviando pacote de dados com cabecalho: " + res + "/" + str(len(pacotes))				    
+		servidorSocket.sendto(res, end)
 
-	look.release()
-
-servidorSocket = socket(AF_INET, SOCK_DGRAM)
+# Uso do signal para setar alarme do timeout
 signal.signal(signal.SIGALRM, definirReenvio)
 
-
-# A mensagem sera dividida no tamanho da janela e salva em um vetor de string, o indice do vetor vai representar o numero de sequencia
+# Nome da funcao: mySendTo()
+# Objetivo: vai determinar apartir da probabilidade se deve ocorrer perda, corrupcao ou envio normal
+def mySendTo(nroSeq, res, enderecoReceptor, probPerda, probCorrupcao):
+	if(nroSeq != 3):
+		servidorSocket.sendto(res, enderecoReceptor)
+	else:
+		print "Perda do pacote 3"
+		
+# Nome da funcao: dividirMensagem()
+# Objetivo: dividir a a mensagem no tamanho definido do pacote e salvar em um vetor de string, o indice do vetor vai representar o numero de sequencia
 def dividirMensagem(tamanhoPacote, mensagem):
 
 	pacotes = []
@@ -75,130 +94,119 @@ def dividirMensagem(tamanhoPacote, mensagem):
 	return pacotes
 
 def receberAck():
-
+	print " bido ACK "
 	while 1:
 
-		look.acquire()
-
+		global ack
 		mensagem = servidorSocket.recvfrom(2048)[0]
 			
 		global numSeqBase
 		global numSeqMax
-		global i
-		global pacotes
-		
-		global ack		
 
 		parts  = mensagem.split(";")
 		ack = int(parts[1])
-		print "Recebito ACK " + parts[1]
-		
-		# verificar se nao eh necessario fazer uso do >=
-		if (ack == - 1):
-			print "Todos os ACKs recebidos."
-			break
-		if (ack >= numSeqBase):
-			numSeqMax = numSeqMax + (ack - numSeqBase)
-			numSeqBase = ack
-			signal.alarm(timeout)
-		print numSeqBase
-		print numSeqMax
-		print i
+		print "Recebido ACK " + parts[1]
 
-	
-		look.release()
-
-
+		if (len(parts) == 2):
+			print len(parts)		
+			if (ack == (len(pacotes)-1)):
+				print "Todos os ACKs recebidos."
+				break
+			if (ack >= numSeqBase):
+				numSeqMax = numSeqMax + (ack - numSeqBase)
+				numSeqBase = ack
+				if (numSeqMax >= len(pacotes)):
+					numSeqMax = len(pacotes) - 1
+				print numSeqBase
+				print numSeqMax
+				signal.alarm(timeout)
 
 def main():
-	global i
 	global numSeq
 	global numSeqBase
 	global numSeqMax
 	global pacotes
+	global end
 	global ack
 
-	if(len(sys.argv)>1):
+	tamanhoPacote = 5
+	if(len(sys.argv) != 4):
+
+		# Recebimento de parametros
 		numPort = int(sys.argv[1])
-		print numPort
+		tamanhoJanela = int(sys.argv[2])
+		probPerda = float(sys.argv[3])
+		probCorrupcao = float(sys.argv[4])		
+
+		# Criacao de conexao com a porta informada
 		servidorSocket.bind(('', numPort))
 
-		t_receptor = threading.Thread(target = receberAck)
-		t_receptor.daemon = True
 	
 		while 1:
 
-		
 			print "O servidor esta pronto para receber."
 			mensagem, enderecoReceptor = servidorSocket.recvfrom(2048)
-			print mensagem
-	
+			end = enderecoReceptor
+
+			# Criacao de Thread que ficara executando a funcao receberAck em segundo plano
+			t_receptor = threading.Thread(target = receberAck)
+			t_receptor.daemon = True			
+
 			try:
 				arquivo = open(mensagem, 'r')
-				print mensagem
+				print "Arquivo solicitado: " + mensagem
 				mensagem = arquivo.read()
-				# tratar arquivo nao encontrado aqui
-				
+
+				# Inicia a thread
+	
 				t_receptor.start()
-				ack = 0
+				
+				# Inicia valores dos numeros de sequencias
 				numSeqBase = 0
 				numSeq = 0
-				i = 0
 				numSeqMax = tamanhoJanela - 1
+				ack = -1
 	
 				pacotes = dividirMensagem(tamanhoPacote, mensagem)
-	
-				#eh criado uma thread para receber os acks do receptor
-	
+				
+				# O timeout eh usado para decidir quando ocorrerá o reenvio	
 				signal.alarm(timeout)
 	
-				#pacote eh transmitido em ordem
+				# Transmissao dos pacotes em ordem
 				while 1:
-					if (i == (len(pacotes) - 1)):
-						print numSeq
+					if (numSeq == (len(pacotes)) and ack == (len(pacotes)-1)):
 						break
 
+					# Só envia a mensagem quando o numSeq não atingiu o tamanho da janela
 					if (numSeq <= numSeqMax):
-				
-   						pacoteSemCheckSum = str(numSeq) + ";" + pacotes[i] + ";"
-						valorCheckSum = checksum(pacoteSemCheckSum, 0)
-						res = str(valorCheckSum) + ";" + pacoteSemCheckSum
-						# [melhorar depois] mensagem avisando que foi enviado o pacote
+
+						res = gerarMensagem(numSeq, pacotes[numSeq])
 						print "Enviando pacote de dados com cabecalho: " + res + "/" + str(len(pacotes))				    
-						servidorSocket.sendto(res, enderecoReceptor)
+						mySendTo(numSeq, res, enderecoReceptor, probPerda, probCorrupcao)
 	
 						numSeq += 1
-						i += 1
-					#quando atinge o tamanho da janela ele deve reenviar os pacotes
-					#else:
-						#time
-						#print "Tamanho da janela atingido " + str(i)
-						#print ack
-						#i = ack + 1
-						#numSeq = i
-					#print i
-					#print numSeq
-	
+
+				# Quando o arquivo acaba enviamos o numero de sequencia igual a -1 para que o receptor identifique esse termino
+				
+				t_receptor.join()
+				signal.alarm(0)
 				numSeq = -1
-				pacoteSemCheckSum = str(numSeq) + ";" + pacotes[i] + ";"
-				valorCheckSum = checksum(pacoteSemCheckSum, 0)
-				res = str(valorCheckSum) + ";" + pacoteSemCheckSum
+				res = gerarMensagem(numSeq, "FIM")	
+				print "Enviando pacote de dados de finalizacao da conexão."				    			
 				servidorSocket.sendto(res, enderecoReceptor)
 				arquivo.close()
-				
-			except IOError:	
-				numSeq = -1
-				pacoteSemCheckSum = str(numSeq) + ";Arquivo nao encontrado"
-				valorCheckSum = checksum(pacoteSemCheckSum, 0)
-				res = str(valorCheckSum) + ";" + pacoteSemCheckSum
-				servidorSocket.sendto(res, enderecoReceptor)
-	 
-				print "Arquivo solicitado nao encontrado."
-						
 
-			t_receptor.join()
+			# Quando o arquivo solicitado não existe eh enviado para o receptor o numero de sequencia igual a -1 e a mensagem de arquivo nao encontrado
+			except IOError:	
+
+				
+				numSeq = -1
+				res = gerarMensagem(numSeq, ";Arquivo nao encontrado")
+				print "Arquivo solicitado nao encontrado."	
+				servidorSocket.sendto(res, enderecoReceptor)
 						
-		else:
-			print "Espera-se o seguinte parametro: numero de porta do servico"
+						
+	else:
+		print "Espera-se o seguinte parametro: numero de porta do servico, tamanho da janela, probabilidade de perda e probabilidade de corrupcao."
 
 main()
